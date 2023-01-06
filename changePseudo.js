1f7617cbf225227adc614d45bb5d1f9755a519b7
Guillim <guigloo@msn.com>
2023-01-06 01:44:02 +0100

    working version

--- changePseudo.js
+++ changePseudo.js
@@ -1,58 +1,109 @@
+// making async some function with this helper
+const readLocalStorage = async (key) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], function (result) {
+      if (result[key] === undefined) {
+        reject();
+      } else {
+        resolve(result[key]);
+      }
+    });
+  });
+};
 
 var w2pclick = document.getElementById('w2pclick');
 var apiupdate = document.getElementById('apiupdate');
+var corsupdate = document.getElementById('corsupdate');
+var n8nupdate = document.getElementById('n8nupdate');
 
-var corsURL = ''
-var n8nURL = ''
-
-fetchID = function(secretAPIKEY){
-  var id = document.getElementById('addPseudo');
+fetchID = async function(api,n8nURL,corsURL){
+  var id = document.getElementById('idtoinspect');
   id &&= id.value;
-  const myHeaders = new Headers();
-  myHeaders.append('X-API-KEY',secretAPIKEY);
+  const myHeaders = await new Headers();
+  myHeaders.append('X-API-KEY',api);
   myHeaders.append("X-Requested-With",n8nURL);
   myHeaders.append("Content-Type", "text/plain");
-
-  fetch(corsURL+id,{
+  
+  const response = await fetch(corsURL+id,{
     method: 'GET',
     mode: 'cors',
     cache: 'default',
     headers:myHeaders
   })
-  .then(res => {document.getElementById('code_for_json').innerHTML = JSON.stringify(res)})
-  .catch(err => {document.getElementById('code_for_json').innerHTML = JSON.stringify(err)})
+
+  // const reader = response.body.getReader();
+  const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+
+  let obj
+  while (true) {
+    const {value, done} = await reader.read();
+    
+    if (done) break;
+    obj = value
+  }
+  document.getElementById('code_for_json').innerHTML = JSON.stringify(JSON.parse(obj), null, "  ")
+
+  
 }
 
 
-
-w2pclick.onclick = function() {
-  var id = document.getElementById('addPseudo');
+w2pclick.onclick = async function() {
+  var id = document.getElementById('idtoinspect');
   id &&= id.value;
+  chrome.storage.local.set({ idtoinspect: id }).then(() => {    console.log("i updated");  });
+
+  const n8n = await readLocalStorage('n8n');
+  const cors = await readLocalStorage('cors');
+  const api = await readLocalStorage('api');
 
   document.getElementById('code_for_json').innerHTML = ''
 
-  chrome.storage.local.get(["secretAPIKEY"]).then((result) => {
-    return result.secretAPIKEY; 
-  }).then((key) => {
-    return fetchID(key)
-  })
+  await fetchID(api,n8n,cors)
 }
+
+
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
 
 apiupdate.onclick = function() {
-  var id = document.getElementById('secretAPIKEY');
+  var id = document.getElementById('api');
   id &&= id.value;
- 
-  chrome.storage.local.set({ secretAPIKEY: id }).then(() => {
-    console.log("Value is updated");
-  });
+  chrome.storage.local.set({ api: id }).then(() => {    console.log("a updated");  });
 }
+
+n8nupdate.onclick = function() {
+  var id = document.getElementById('n8n');
+  id &&= id.value;
+  chrome.storage.local.set({ n8n: id }).then(() => {    console.log("n updated");  });
+}
+
+
+corsupdate.onclick = function() {
+  var id = document.getElementById('cors');
+  id &&= id.value;
+  chrome.storage.local.set({ cors: id }).then(() => {    console.log("c updated");  });
+}
+
+
 
 initialTrigger = function(){
-  chrome.storage.local.get(["secretAPIKEY"]).then((result) => {
-    var id = document.getElementById('secretAPIKEY');
-    id.defaultValue = result.secretAPIKEY; 
+  chrome.storage.local.get(["api"]).then((result) => {
+    document.getElementById('api').defaultValue = result.api; 
   });
-}
+  chrome.storage.local.get(["n8n"]).then((result) => {
+    document.getElementById('n8n').defaultValue = result.n8n; 
+  });
+  chrome.storage.local.get(["cors"]).then((result) => {
+    document.getElementById('cors').defaultValue = result.cors; 
+  });
+  chrome.storage.local.get(["idtoinspect"]).then((result) => {
+    document.getElementById('idtoinspect').defaultValue = result.idtoinspect; 
+  });
 
+}
 
 initialTrigger()
